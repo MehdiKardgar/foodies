6a043b1fca2efecf589d3dbcc2a37c456092563b
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2024-04-29 23:15:48 +0330

    outputting meal data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,29 @@
+// Here I wanna output a bunch of meals
+
+import Link from "next/link";
+
+import classes from "./page.module.css";
+import MealsGrid from "@/component/meals/meals-grid";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. it is seasy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
