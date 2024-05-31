bc5cf15189bb433a80042bb46c3460eb790c5b6f
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2024-05-31 17:34:27 +0330

    Not Found State
    Handling Not Found States

---
+++ app/not-found.js
@@ -0,0 +1,19 @@
+// not-found.js
+
+// Summary: This code defines a React component for handling 404 (Not Found) errors.
+// When a user accesses a route that doesn't exist, this component displays an error message and provides a link to go back to the homepage.
+
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Oops! Page Not Found</h1>
+      <p> The requested page does not exist (404).</p>
+
+      <p>
+        <Link href="/">Go back to the homepage</Link>
+      </p>
+    </main>
+  );
+}
